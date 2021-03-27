62c3bd130ad2e4aa6743f49b598c075e2898f145
fedy97 <frc.morreale@gmail.com>
2021-03-27 18:24:29 +0100

    added visitor pattern

--- src/visitor/Demo.java
+++ src/visitor/Demo.java
@@ -1,5 +1,12 @@
 package visitor;
 
+/**
+ * with this pattern we assume we will not implement other Segments,
+ * but only new filters on the segments. In this way our code will comply with
+ * open-close principle.
+ * Overloading used in the apply method.
+ */
+
 public class Demo {
     public static void show() {
         var wavFile = WavFile.read("myfile.wav");
