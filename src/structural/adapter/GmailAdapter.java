2075af544c1ff6deaa2b4b301c480a6962cbde49
fedy97 <frc.morreale@gmail.com>
2021-03-29 16:52:50 +0200

    added adapter and composite patterns

---
+++ src/structural/adapter/GmailAdapter.java
@@ -0,0 +1,13 @@
+package structural.adapter;
+import structural.adapter.Gmail.GmailClient;
+
+public class GmailAdapter implements EmailProvider {
+  private GmailClient client = new GmailClient();
+
+  @Override
+  public void downloadEmails() {
+    client.connect();
+    client.getEmails();
+    client.disconnect();
+  }
+}
